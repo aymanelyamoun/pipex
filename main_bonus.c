9e94321ca61b01cd32a0f0bc2884823f031f3158
Aymane El-yamoun <ael-yamo@e2r4p8.1337.ma>
2022-04-17 23:35:20 +0000

    deleting unnecssary comments

--- main_bonus.c
+++ main_bonus.c
@@ -48,7 +48,6 @@ void	other_childs(int **fd, int fd_input, int fd_output, t_cmd *cmds, int pipe_c
 	close_pipes(fd, pipe_count);
 	close(fd_output);
 	close(fd_input);
-	// perror("i am an other child\n");
 	execve(cmds[i].cmd_path, cmds[i].cmd_args, cmds[i].envp);
 	perror("something whent wrogn with execve\n");
 }
@@ -60,7 +59,6 @@ void	last_child(int **fd, int fd_input, int fd_output, t_cmd *cmds, int pipe_cou
 	close_pipes(fd, pipe_count);
 	close(fd_output);
 	close(fd_input);
-	// perror("i am the last child\n");
 	execve(cmds[i].cmd_path, cmds[i].cmd_args, cmds[i].envp);
 	perror("somthing whent wrogn with execve\n");
 }
@@ -128,7 +126,6 @@ void	pipex(int fd_input, int fd_output, t_main_args args)
 	close_pipes(pipes, args.argc - 4);
 	close(fd_output);
 	close(fd_input);
-	
 	waitpid(id_1, 0, 0);
 	while(waitpid(-1, 0, 0) >= 0);
 }
@@ -142,24 +139,7 @@ t_main_args	set_args(int argc, char **argv, char **envp)
 	args.envp = envp;
 	return (args);
 }
-// void	print_cmds(t_cmd *cmds, t_main_args args)
-// {
-// 	int i;
-// 	int	j;
 
-// 	i = 0;
-// 	while (i < args.argc - 3)
-// 	{
-// 		j = 0;
-// 		printf("cmd path : %s\n", cmds->cmd_path);
-// 		while (cmds[i].cmd_args[j] != NULL)
-// 		{
-// 			printf("cmd args : %s\n", cmds[i].cmd_args[j]);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
 int main(int argc, char **argv, char **envp)
 {
 	int	fd_input;
